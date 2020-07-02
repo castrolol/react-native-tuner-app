f1f3055e21393a7ca84f3b111b67e33f0a5fb142
castrolol <luan.castro@nuvem.net>
2020-07-02 00:58:22 -0400

    remove unecessary tests

--- src/screens/__tests__/TunerScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render, getNodeText} from '@testing-library/react-native';
-import TunerScreen from '../TunerScreen';
-
-test('Deve renderizar', () => {
-  render(<TunerScreen />);
-});
